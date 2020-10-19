d65ad73e48f33e93a2da3cc91a48d5d021707ead
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-19 21:14:23 +0100

    ADD basic server

---
+++ server.go
@@ -0,0 +1,19 @@
+// server.go
+
+package server
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Welcome to Identico!")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", welcome)
+	http.ListenAndServe(":"+port, nil)
+}

9aabbea67a88f0ba73756ed34df16dc801817940
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-19 22:17:11 +0100

    TEST svg package

---
+++ svg/svg_test.go
@@ -0,0 +1,23 @@
+// svg/svg_test.go
+
+package svg
+
+import "testing"
+
+func TestToString(t *testing.T) {
+	svgElement := New()
+	expectedSVG := `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 8" shape-rendering="crispEdges"></svg>`
+	if svgElement.ToString() != expectedSVG {
+		t.Errorf("default svg ToString should be: %v , instead got: %v", expectedSVG, svgElement.ToString())
+	}
+}
+
+func TestAddElementAndToString(t *testing.T) {
+	svgElement := New()
+	svgElement.AddElement(newRectElement("testColor", 0, 0, 1, 1))
+
+	expectedSVG := `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 7 8" shape-rendering="crispEdges"><rect fill="testColor" x="0" y="0" width="1" height="1" /></svg>`
+	if svgElement.ToString() != expectedSVG {
+		t.Errorf("svg with inner element ToString should be: %v , instead got: %v", expectedSVG, svgElement.ToString())
+	}
+}

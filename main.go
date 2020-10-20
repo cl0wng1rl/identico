e055b846e68d95648506e717bb6a3370a2a03ad4
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-21 00:34:05 +0100

    PREVENT caching
    This may be reverted when url params are introduced

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 	id := identicon.SimpleIdenticon{}
 	s := id.Create(currentTimeString)
 	w.Header().Set("Content-Type", "image/svg+xml")
+	w.Header().Set("Cache-Control", "no-store")
 	io.WriteString(w, s.ToString())
 }
 

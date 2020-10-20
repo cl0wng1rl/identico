b4cb8af429127cf9890b6e748029d6993f5c2a18
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-20 23:59:45 +0100

    USE SimpleIdenticon as main svg

--- main.go
+++ main.go
@@ -6,17 +6,15 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 
-	"github.com/gabrielbarker/identico/svg"
+	"github.com/gabrielbarker/identico/identicon"
 )
 
 func welcome(w http.ResponseWriter, r *http.Request) {
-	s := svg.New()
-	for i := 0; i < 16; i++ {
-		for j := 0; j < 16; j++ {
-			s.AddElement(svg.NewRandomSquare(i, j))
-		}
-	}
+	currentTimeString := time.Now().Format("2006-01-02 15:04:05 Monday")
+	id := identicon.SimpleIdenticon{}
+	s := id.Create(currentTimeString)
 	w.Header().Set("Content-Type", "image/svg+xml")
 	io.WriteString(w, s.ToString())
 }

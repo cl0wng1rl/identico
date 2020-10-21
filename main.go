684beae0c6b7a2fc2477e9002678ab34dd9d78a4
Gabriel Barker <gabrielbarker@gmail.com>
2020-10-21 15:14:06 +0100

    ADD url parameters for message and quadSize

--- main.go
+++ main.go
@@ -5,17 +5,21 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/gabrielbarker/identico/identicon"
 )
 
 func welcome(w http.ResponseWriter, r *http.Request) {
-	currentTimeString := fmt.Sprintf("%v", time.Now().UnixNano())
+	message := getMessageParameter(r)
+	quadrantSize := getQuadrantSizeParameter(r)
+
 	id := identicon.SimpleIdenticon{}
-	s := id.Create(currentTimeString)
+	s := id.Create(message, quadrantSize)
 	w.Header().Set("Content-Type", "image/svg+xml")
 	w.Header().Set("Cache-Control", "no-store")
 	io.WriteString(w, s.ToString())
@@ -25,4 +29,24 @@ func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/", welcome)
 	http.ListenAndServe(":"+port, nil)
+}
+
+func getMessageParameter(r *http.Request) string {
+	return getURLParameter("message", fmt.Sprintf("%v", time.Now().UnixNano()), r)
+}
+
+func getQuadrantSizeParameter(r *http.Request) int {
+	size, err := strconv.Atoi(getURLParameter("quadrant", "8", r))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return size
+}
+
+func getURLParameter(parameter string, defaultValue string, r *http.Request) string {
+	params, ok := r.URL.Query()[parameter]
+	if !ok || len(params[0]) < 1 {
+		return defaultValue
+	}
+	return params[0]
 }
